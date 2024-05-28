d170a725d3906d73cad50c210897e29e5d705917
thiwakonsakunchao <thiwakon_sakunchao@cmu.ac.th>
2024-05-28 19:24:03 +0700

    fix compose

--- index.js
+++ index.js
@@ -18,28 +18,21 @@ app.use(
 
 
 const port = 8000;
-const secret = "mysecret";
+const secret = process.env.SECRET || "mysecret";
+require('dotenv').config()
 
 let conn = null;
 
 // function init connection mysql
-// const initMySQL = async () => {
-//   conn = await mysql.createConnection({
-//     host: "127.0.0.1",
-//     user: "root",
-//     password: "root",
-//     database: "tutorial",
-//     port: "3306"
-//   });
-// };
-
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'tutorial'
-})
-
+const initMySQL = async () => {
+  conn = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT // Added port configuration
+  });
+};
 
 // อันนี้ลองregister
 app.post('/api/register', async (req,res) => {
